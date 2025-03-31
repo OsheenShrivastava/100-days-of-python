f699553ea2e029258da791669c3da4ae83a21d35
OsheenShrivastava <osheenwork01@gmail.com>
2025-03-31 15:34:27 +0530

    Added File

--- Day_26_The_NATO_Alphabet_Project/main.py
+++ Day_26_The_NATO_Alphabet_Project/main.py
@@ -7,18 +7,8 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # {"A": "Alfa", "B": "Bravo"}
 
 dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(dict)
 
 # TODO-2 - Create a list of the phonetic code words from a word that the user inputs.
-
-# Method 1 using dataframe comprehension without exceptions
-
-# word = input("Enter a word: ").upper()
-# output_list = [dict[letter] for letter in word]
-# print(output_list)
-
-# Method 2 using dataframe comprehension using exceptions
-
 
 def generate_phonetic():
     word = input("Enter a word: ").upper()
