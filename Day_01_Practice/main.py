742801351482eb88fd3c896b5b19ece55539fcbe
OsheenShrivastava <osheenwork01@gmail.com>
2025-10-19 16:02:53 +0530

    Day 1 Practice Added File

---
+++ Day_01_Practice/main.py
@@ -0,0 +1,26 @@
+
+# Printing Strings
+print("Hello World!")
+
+# New Line "/n"
+print("Hello World!\nHello World!")
+
+# Concatinate Strings
+print("Hello" + " World!")
+print("Hello" + " " + "World!")
+
+# Input Function - input() function will take an input from user
+print("Hello " + input("What is your name? ") + "!")
+
+# Variables
+name = "Osheen"
+print(name)
+
+username = input("What is your name? ")
+length = len(username)
+print(length)
+
+# Variable naming
+username_of_this_person = "Osheen"
+length = len(username_of_this_person)
+print(length)
