6d94f47cb59f4deb5c46b98587b774a6c22b3abc
OsheenShrivastava <osheenwork01@gmail.com>
2025-10-23 15:30:03 +0530

    Day 3 Practice File Added

---
+++ Day_03_Practice/Output.md
@@ -0,0 +1,23 @@
+<!-- Welcome to the rollercoaster!
+What is your height in cm? 121
+You can ride the rollercoaster
+
+
+How many numbers would you like to check? 25
+The number is odd
+
+
+Welcome to Python Pizza Deliveries!
+What size pizza do you want? S, M or L: L
+Do you want pepperoni on your pizza? Y or N: Y
+Do you want extra cheese? Y or N: Y
+Your final bill is: $29.
+
+
+Welcome to the rollercoaster!
+What is your height in cm? 121
+You can ride the rollercoaster
+What is your age? 25
+Adult tickets are $12.
+Do you want to have a photo taken? Type y for Yes and n for No.Y
+Your final bill is: 12 -->
---
+++ Day_03_Practice/main.py
@@ -0,0 +1,120 @@
+
+# If,Else and Conditional Operators
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:  # >= and == can also be checked
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
+print("\n")
+
+# Modulo Operator
+number_to_check = int(input("How many numbers would you like to check? "))
+
+if number_to_check % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+print("\n")
+
+# Nested if and elif statements
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+#
+# if height > 120:
+#     print("You can ride the rollercoaster")
+#     age = int(input("What is your age? "))
+#     if age <= 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry you have to grow taller before you can ride.")
+
+# Multiple if statements
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+#
+# if height > 120:
+#     print("You can ride the rollercoaster")
+#     age = int(input("What is your age? "))
+#     if age <= 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+#
+#     wants_photo = input("Do you want to have a photo taken? Type y for Yes and n for No.")
+#     if wants_photo == "y":
+#         bill += 3
+#
+#     print(f"Your final bill is: {bill}")
+# else:
+#     print("Sorry you have to grow taller before you can ride.")
+
+# Pizza Order Practice
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("You typed the wrong inputs.")
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
+
+print("\n")
+
+# Logical Operators
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height > 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be OK. Have a free ride on us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want to have a photo taken? Type y for Yes and n for No.")
+    if wants_photo == "y":
+        bill += 3
+
+    print(f"Your final bill is: {bill}")
+else:
+    print("Sorry you have to grow taller before you can ride.")
