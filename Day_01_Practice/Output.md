20a2736fdaa2d07839b67b3ac6e9f48ed06bc88e
OsheenShrivastava <osheenwork01@gmail.com>
2026-04-15 19:13:06 +0530

    Day 1 Practice File Added

--- Day_01_Practice/Output.md
+++ Day_01_Practice/Output.md
@@ -1,11 +1,23 @@
 <!-- Hello World!
+
+
 Hello World!
 Hello World!
+
+
 Hello World!
 Hello World!
+
+
 What is your name? Osheen
 Hello Osheen!
+
+
 Osheen
+
+
 What is your name? Osheen
 6
+
+
 6 -->
--- Day_01_Practice/main.py
+++ Day_01_Practice/main.py
@@ -1,24 +1,30 @@
 
 # Printing Strings
 print("Hello World!")
+print("\n")
 
 # New Line "/n"
 print("Hello World!\nHello World!")
+print("\n")
 
 # Concatinate Strings
 print("Hello" + " World!")
 print("Hello" + " " + "World!")
+print("\n")
 
 # Input Function - input() function will take an input from user
 print("Hello " + input("What is your name? ") + "!")
+print("\n")
 
 # Variables
 name = "Osheen"
 print(name)
+print("\n")
 
 username = input("What is your name? ")
 length = len(username)
 print(length)
+print("\n")
 
 # Variable naming
 username_of_this_person = "Osheen"
