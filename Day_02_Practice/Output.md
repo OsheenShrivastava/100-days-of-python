4c7cf540e19ee598833c6938f1b3d96d3de20812
OsheenShrivastava <osheenwork01@gmail.com>
2026-04-15 19:09:59 +0530

    Day 2 Practice File Added

--- Day_02_Practice/Output.md
+++ Day_02_Practice/Output.md
@@ -1,12 +1,34 @@
 <!-- H
+
+
 123345
+
+
 468
+
+
 123456789
+
+
 3.14159
+
+
 True
 False
+
+
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+
+
 Enter your name Osheen
+<class 'str'>
+<class 'int'>
 Number of characters in your name: 6
+
+
 579
 4
 6
@@ -14,9 +36,13 @@ Number of characters in your name: 6
 1
 8
 7.0
+
+
 30.85399449035813
 30
 31
 30.85
+
+
 1
 Your score is 1 -->
--- Day_02_Practice/main.py
+++ Day_02_Practice/main.py
@@ -1,22 +1,28 @@
 
 # Subscripting
 print("Hello"[0])
+print("\n")
 
 # Strings
 print("123" + "345")
+print("\n")
 
 # Integers
 print(123 + 345)
+print("\n")
 
 # Large Integers
 print(123456789)
+print("\n")
 
 # Floats
 print(3.14159)
+print("\n")
 
 # Boolean
 print(True)
 print(False)
+print("\n")
 
 # Type Error
 # len(12345)
@@ -26,6 +32,7 @@ print(type("Hello"))
 print(type(123))
 print(type(3.14))
 print(type(True))
+print("\n")
 
 # Type Conversion
 name_of_the_user = input("Enter your name ")
@@ -35,6 +42,7 @@ print(type("Number of letters in your name: "))
 print(type(length_of_name))
 
 print("Number of characters in your name: " + str(length_of_name))
+print("\n")
 
 # Mathematical Operations - PEMDAS - () ** * / + -
 print(123 + 456)
@@ -45,6 +53,7 @@ print(5 // 3)
 print(2 ** 3)
 
 print(3 * 3 + 3 / 3 - 3)
+print("\n")
 
 # round function
 bmi = 84 / 1.65 ** 2
@@ -53,6 +62,7 @@ print(bmi)
 print(int(bmi))
 print(round(bmi))
 print(round(bmi, 2))
+print("\n")
 
 # f-strings
 score = 0
