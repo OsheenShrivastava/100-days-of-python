9f64497172008a194dc04d022cc03a54eb7c9847
OsheenShrivastava <osheenwork01@gmail.com>
2026-04-30 18:55:27 +0530

    Day 18 Practice File Added

---
+++ Day_18_Practice/main.py
@@ -0,0 +1,128 @@
+
+# Turtle Graphics
+
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+# Draw a Square
+
+tim = Turtle()
+
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+
+# Working with aliases
+
+import turtle as t
+
+tim = t.Turtle()
+
+# Draw a Dashed Line
+
+import turtle as t
+
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+# Drawing different shapes
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 10):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
+
+# Generate a Random Walk
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+# Generate Random RGB
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+# Draw a Spirograph
+
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
+
+
+screen = Screen()
+screen.exitonclick()
